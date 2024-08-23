6cdd38910168ff91298a5d83e29aabffbb93134f
Huang-Tony <tonyh2027@student.cis.edu.hk>
2024-08-23 12:33:38 +0800

    v1 lab completed

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner inputage = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = inputage.nextInt();
+        int newage = age/2 + 7;
+        System.out.println("The youngest person you can date is: " + newage);
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner inputage = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = inputage.nextInt();
+        int page = 100 - age;
+        System.out.println("You should read " + page + " pages.");
     }
 }
